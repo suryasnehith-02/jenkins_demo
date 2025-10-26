06d991afd6b71a73de5dfaadd3b9310830fcf07a
Surya <narlasurya2189@gmail.com>
2025-10-26 10:58:45 +0530

    d

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Docker! 🎉</h1><p>This is a simple Flask app running </p>"
+    return "<h1>Hello from Docker! 🎉</h1><p>This is a simple Flask app running ia to be build and should be running by surya </p>"
 
 if __name__ == "__main__":
     # For development only: Flask built-in server (works fine for this simple demo).
